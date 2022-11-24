d13af82a1cadf247c7a5d366a3016be0a7535d4c
dlcoder224 <liuwenlong@111.com>
2022-11-24 23:27:03 +0800

    feat：更换接口地址

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 const app: Express = express()
 
 // 支持跨域问题
-app.use('*', (req,res,next) => {
+app.use('*', (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
 	next()
 })
@@ -15,17 +15,17 @@ const router: Router = express.Router()
 app.use('/api', router)
 
 router.get('/list', async (req: Request, res: Response) => {
-  // 腾讯疫情接口模拟数据
-  const result = await axios.post(
-    'https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=nowConfirmStatis,provinceCompare'
-  )
-  // 返回json格式数据
-  res.json({
-    data: result.data
-  })
+	// 腾讯疫情接口模拟数据
+	const result = await axios.post(
+		'https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=staticGradeCityDetail,diseaseh5Shelf'
+	)
+	// 返回json格式数据
+	res.json({
+		...result.data.data
+	})
 })
 
 // 启动项目端口
 app.listen(3333, () => {
-  console.log('success server http://localhost:3333')
+	console.log('success server http://localhost:3333')
 })
